6243dbbe8c095beec5f08ee3ad7b9f54dc796ae9
MaramMiled <maram.miled003@gmail.com>
2026-04-13 12:02:06 +0100

    Initital commit

---
+++ AppTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using IncidentAPI_X.Models;
+
+namespace AppTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Testing");
+
+            builder.ConfigureServices(services =>
+            {
+                // ✅ Remove existing DbContext
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<IncidentsDbContext>));
+
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                // ✅ Add test DbContext
+                services.AddDbContext<IncidentsDbContext>(options =>
+                    options.UseSqlServer(
+                        "Server=(localdb)\\mssqllocaldb;Database=IncidentDb_Test;Trusted_Connection=True;TrustServerCertificate=True;"
+                    )
+                );
+
+                // ✅ Build provider
+                var sp = services.BuildServiceProvider();
+
+                // ✅ Initialize database
+                using (var scope = sp.CreateScope())
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<IncidentsDbContext>();
+
+                    db.Database.EnsureDeleted();
+                    db.Database.EnsureCreated();
+                }
+            });
+        }
+    }
+}
